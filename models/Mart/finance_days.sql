300965401030a62bb16aa2576e6dcfe97bbea826
martinmathysda@gmail.com <martinmathysda@gmail.com>
2024-02-07 14:40:49 +0000

    commit

--- models/Mart/finance_days.sql
+++ models/Mart/finance_days.sql
@@ -1,9 +1,9 @@
 SELECT
 date_date,
-COUNT (orders_id),
+COUNT (orders_id) AS nb_transactions,
 SUM(revenue),
-COUNT(orders_id) / SUM(revenue) AS average_basket,
-margin,
-operational_margin
+(COUNT(quantity) / SUM(revenue)) AS average_basket,
+SUM(margin) AS margin,
+SUM(operational_margin) AS operational margin
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
