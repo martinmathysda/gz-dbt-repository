d87601aec7cc5c36ca8da73f51cceb44f9752182
martinmathysda@gmail.com <martinmathysda@gmail.com>
2024-02-07 15:55:17 +0000

    add create table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -5,6 +5,6 @@ revenue,
 margin,
 quantity,
 CAST(margin AS float64) + CAST(shipping_fee AS float64) - CAST(logcost AS float64) - CAST(ship_cost AS float64) AS operational_margin
-FROM {{ref("int_orders_margin")}}
+FROM {{ref("int_sales_margin")}}
 JOIN {{ref("stg_raw__ship")}}
 USING (orders_id)
